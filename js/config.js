01e587828294295159e91ded0defdb10d4bfecb8
Giannina Baccelliere <13427145-giannina.baccelliere@users.noreply.gitlab.com>
2023-03-01 10:59:42 -0300

    fix: configurin config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_UJuew4mXh', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 't7ka9pd2i5jfnb50c5c8n66ku', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'us-west-2_Sl5iWyo1H', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '30qob3f3qh90s5ggka26ve48ob', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
